1e8952be10c0ccf30872ea04d17fc580f15c10fb
Thuận Phát <phat06112003@gmail.com>
2023-08-14 18:35:49 +0700

    sua homepage

--- fe/src/pages/home/home.jsx
+++ fe/src/pages/home/home.jsx
@@ -1,20 +1,191 @@
 import './home.css'
 import { Link } from 'react-router-dom'
-import '../../assets/fontawesome-icon/css/all.css'
+import { FaEye } from "react-icons/fa";
+import { MdModeComment } from "react-icons/md";
+import { AiFillStar } from "react-icons/ai";
 
 export default function Home() {
+  const exampleTable = [
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    }
+  ]
+
+  const exampleSection = [
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    },
+    {
+      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
+      name: 'Chainsaw man (this is a really good comic)',
+      view: '1.234K',
+      comment: '1.234K',
+      rating: 4.8 
+    }
+  ]
+
   return (
     <div className="homepage-container">
       <div className="homepage-body">
-        <Section title="Top truyện hot"></Section>
-        <Section title="Truyện mới"></Section>
-        <Section title="Tác giả mới"></Section>
+        <Section title="Danh Sách Truyện" data={exampleSection}></Section>
+        <span className='tableSection'>
+        <Table name="Bảng Xếp Hạng" data={exampleTable}></Table>
+        <Table name="Truyện Đang Theo Dõi" data={exampleTable}></Table>
+        </span>
       </div>
     </div>
   )
 }
 
-function Section({ title }) {
+function Section({ title, data }) {
   const in4 = {
     img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
     name: 'Chainsaw man (this is a really good comic)',
@@ -27,12 +198,9 @@ function Section({ title }) {
     <div className="section">
       <h3 className="section-title">{title + ' >'}</h3>
       <div className="section-slider">
-        <Comic data={in4}></Comic>
-        <Comic data={in4}></Comic>
-        <Comic data={in4}></Comic>
-        <Comic data={in4}></Comic>
-        <Comic data={in4}></Comic>
-        <Comic data={in4}></Comic>
+      {
+        data.map((cur, index) => (<Comic key={index} data={cur}></Comic>))
+      }
       </div>
     </div>
   )
@@ -45,21 +213,56 @@ function Comic({data}) {
     <div className="section-comic-info">
       <h3 className="section-comic-name">{data.name}</h3>
       <div className="section-comic-stats">
-        <div className="section-stat-wrap">
-          <i className="fa-solid fa-eye section-stat-icon"></i>
-          <h4 className="section-stat-number">{data.view}</h4>
-        </div>
-        <div className="section-stat-wrap">
-          <i className="fa-solid fa-comment section-stat-icon"></i>
-          <h4 className="section-stat-number">{data.comment}</h4>
-        </div>
-        <div className="section-stat-wrap">
-          <i className="fa-solid fa-star section-stat-icon"></i>
-          <h4 className="section-stat-number">{data.rating}</h4>
-        </div>
+        <span className="section-stat-wrap">
+          <FaEye className="section-stat-icon"/>
+          <h3 className="section-stat-number">{data.view}</h3>
+        </span>
+        <span className="section-stat-wrap">
+          <MdModeComment className="section-stat-icon"/>
+          <h3 className="section-stat-number">{data.comment}</h3>
+        </span>
+        <span className="section-stat-wrap">
+          <AiFillStar className="section-stat-icon"/>
+          <h3 className="section-stat-number">{data.rating}</h3>
+        </span>
       </div>
     </div>
   </Link>
   )
 }
 
+function Table({name, data}){
+  return (
+    <div className="homepage-table-container">
+      <h3 className="homepage-table-name">{name}</h3>
+      {
+        data.map((cur, index) => (<Row key={index} props={cur}></Row>))
+      }
+    </div>
+  )
+}
+
+function Row({ props }){
+  return (
+    <Link className="homepage-row-container" to="">
+      <img className="homepage-row-img" src={props.img} alt="" />
+      <div className="homepage-row-info">
+        <h3 className="homepage-row-name">{props.name}</h3>
+        <span className="homepage-row-stat">
+          <span className="homepage-row-stat-wrap">
+            <FaEye className="homepage-row-stat-icon"/>
+            <h3 className="homepage-row-stat-number">{props.view}</h3>
+          </span>
+          <span className="homepage-row-stat-wrap">
+            <MdModeComment className="homepage-row-stat-icon"/>
+            <h3 className="homepage-row-stat-number">{props.comment}</h3>
+          </span>
+          <span className="homepage-row-stat-wrap">
+            <AiFillStar className="homepage-row-stat-icon"/>
+            <h3 className="homepage-row-stat-number">{props.rating}</h3>
+          </span>
+        </span>
+      </div>
+    </Link>
+  )
+}
