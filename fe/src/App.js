95a1bfb016139617b4203f466cb0104e5bbe8223
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2023-08-14 17:32:30 +0700

    add context to handle login

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/home'
 import Comic from './pages/comic/comic'
 import Follow from './pages/follow/follow'
-import Login from './pages/login/login'
+import { Login } from './pages/login/login'
 import Register from './pages/login/register'
 import MainComic from './pages/main_comic/main_comic'
+import Profile from './pages/profile/profile'
 import Layout from './layout'
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="comic/main-comic" element={<MainComic />} /> 
+          <Route path="profile/Dashboard" element={<Profile />} /> 
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
