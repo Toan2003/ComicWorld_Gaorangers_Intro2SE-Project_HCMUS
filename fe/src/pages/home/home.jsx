e8435178cd353745d0ed228d8f217cfa179c78a5
Thuận Phát <phat06112003@gmail.com>
2023-08-15 19:13:30 +0700

    use data from be

--- fe/src/pages/home/home.jsx
+++ fe/src/pages/home/home.jsx
@@ -12,8 +12,6 @@ export default function Home() {
   async function loadDataPage() {
     const comics = await getAllComic()
     const ranks = await getRankingBoard()
-    console.log(comics)
-    // console.log(ranks)
     setComic(comics.data.data.listComic)
     setRank(ranks.data.data.rankingList)
   }
@@ -21,161 +19,13 @@ export default function Home() {
   useEffect(() => {
     loadDataPage()
   }, [])
-  console.log(comic)
-  // console.log(rank)
-
-  const exampleTable = [
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    }
-  ]
-
-  const exampleSection = [
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    },
-    {
-      img: 'https://i.ex-cdn.com/mgn.vn/files/content/2022/10/09/cover-chainsaw-man-1512.jpeg',
-      name: 'Chainsaw man (thêm dòng này để tên nó dài ra để test css)',
-      view: '1.234K',
-      comment: '1.234K',
-      rating: 4.8 
-    }
-  ]
 
   return (
     <div className="homepage-container">
       <div className="homepage-body">
-        <Section title="Danh Sách Truyện" data={exampleSection}></Section>
+        <Section title="Danh Sách Truyện" data={comic}></Section>
         <span className='tableSection'>
-        <Table name="Bảng Xếp Hạng" data={exampleTable}></Table>
+        <Table name="Bảng Xếp Hạng" data={rank}></Table>
         <Table name="Truyện Đang Theo Dõi" ></Table>
         </span>
       </div>
@@ -199,10 +49,10 @@ function Section({ title, data }) {
 function Comic({data}) {
   return (
     <div className="comic-container">
-      <Link className="section-comic" to=''>
-        <img className="secion-comic-img" src={data.img} alt="" />
+      <Link className="section-comic" to='/comic/main-comic' id={data._id}>
+        <img className="secion-comic-img" src={data.coerURL} alt={data.nameComics} />
         <div className="section-comic-info">
-          <h3 className="section-comic-name">{data.name}</h3>
+          <h3 className="section-comic-name">{data.nameComics}</h3>
           <div className="section-comic-stats">
             <span className="section-stat-wrap">
               <FaEye className="section-stat-icon"/>
@@ -210,7 +60,7 @@ function Comic({data}) {
             </span>
             <span className="section-stat-wrap">
               <AiFillStar className="section-stat-icon"/>
-              <h3 className="section-stat-number">{data.rating}</h3>
+              <h3 className="section-stat-number">{data.ratingAvg}</h3>
             </span>
           </div>
         </div>
@@ -235,21 +85,17 @@ function Table({name, data}){
 function Row({ data }){
   return (
     <Link className="homepage-row-container" to="">
-      <img className="homepage-row-img" src={data.img} alt="" />
+      <img className="homepage-row-img" src={data.coverURL} alt="" />
       <div className="homepage-row-info">
-        <h3 className="homepage-row-name">{data.name}</h3>
+        <h3 className="homepage-row-name">{data.nameComics}</h3>
         <span className="homepage-row-stat">
           <span className="homepage-row-stat-wrap">
             <FaEye className="homepage-row-stat-icon"/>
             <h3 className="homepage-row-stat-number">{data.view}</h3>
           </span>
           <span className="homepage-row-stat-wrap">
-            <MdModeComment className="homepage-row-stat-icon"/>
-            <h3 className="homepage-row-stat-number">{data.comment}</h3>
-          </span>
-          <span className="homepage-row-stat-wrap">
             <AiFillStar className="homepage-row-stat-icon"/>
-            <h3 className="homepage-row-stat-number">{data.rating}</h3>
+            <h3 className="homepage-row-stat-number">{data.ratingAvg}</h3>
           </span>
         </span>
       </div>
