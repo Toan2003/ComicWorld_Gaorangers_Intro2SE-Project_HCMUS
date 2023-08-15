8736eb55728cb25fb32a10b10ebf6fbf49559620
To Khanh Linh <lintoh111@gmail.com>
2023-08-15 20:15:30 +0700

    update new comic va read comic

--- fe/src/pages/comic/comic.jsx
+++ fe/src/pages/comic/comic.jsx
@@ -4,7 +4,9 @@ import React,{useState} from "react"
 
 
 
-function App() {
+function Comic() {
+  const NameComic="Comic Name"
+  const Chapter=0;
   const [selects,setSelects]=useState()
   const [input,setInput]=useState("")
   const [comicImages, setComicImages] = useState([
@@ -14,22 +16,31 @@ function App() {
     './4'
     // ...Thêm các hình ảnh truyện tranh khác vào đây HÌNH phải là đuôi jpg và xóa đuôi nó đi
   ]);
+  const AvatarImage='./avatar'
+  const Username="abc123"
   const [value, setValue] = useState("") //description
   const handleChange = (event) => {
     setValue(event.target.value);
   }
   const handleResize = (event) => {
     event.target.style.height = "auto";
-    event.target.style.height = `${event.target.scrollHeight}px`;
+    event.target.style.height = `250px`;
   }
   return (
     <div className="App">
-      
-      <div className="Background">
-      </div>
       <div className="Logo_background">
       </div>
+      <input type="text"  placeholder='Nhập từ khóa tìm kiếm...' value ={input} onChange={(e)=>setInput(e.target.value)} />
+      < div className="Avatar">
+        <img src={require(AvatarImage+'.jpg')} alt={"Avatar Image"}  class="rounded"/> 
+      </div>
+      <button className="Username"> {Username}</button>
       <div className="Gray_after">
+      </div>
+      <div className='Nav_menu'>
+        <button className='Nav_button' ><FaHome/>Trang chủ</button>&nbsp;&nbsp;
+        <button className='Nav_button' >Thể Loại</button>&nbsp;&nbsp;
+        <button className='Nav_button'  >Trang chủ</button>
       </div>
       <div className="Contrucst">
       </div>
@@ -39,13 +50,9 @@ function App() {
       <button className="text-button1">Comic Name</button> &gt;&gt;
       <button className="text-button1">Chapter</button> 
       </div>
-      <div className="Title">
-      Comic Name
-      </div>
+      <number className="Title"> {NameComic} </number> 
       <div className="Pass">&gt;&gt;</div>
-      <div className="Chap">
-      Chapter 0
-        </div>
+      <button className="Chap"> Chapter {Chapter} </button>
         <div className="Rectangle_move"></div>
         <div className="Text_instruct">Sử dụng mũi tên ( &lt;- ) hoặc (-&gt; ) để chuyển chapter ( ^.^ ) </div>
         <div className="icon_button">
@@ -57,7 +64,6 @@ function App() {
             <option>Chap 2</option>
             <option>Chap 3</option>
             <option>Chap 4</option>
-            
           </select>
           &nbsp; &nbsp;
           <button type="button" class="btnbtn-danger" ><FaChevronRight size={25} color='white'/></button> &nbsp;
@@ -74,7 +80,6 @@ function App() {
         <div className="nav_end_chap">
         <button type="button" class="btnbtn-danger" ><FaChevronLeft size={25} color='white'/> Previous Chapter</button>&nbsp;&nbsp;
         <button type="button" class="btnbtn-danger" > Next Chapter <FaChevronRight size={25} color='white'/></button>
-        
         </div>
         <div className="nav_end">
         <button type="button" className="text-button1">Home</button> &gt;&gt;
@@ -82,13 +87,13 @@ function App() {
         <button type="button" className="text-button1">Comic Name</button> &gt;&gt;
         <button type="button" className="text-button1">Chapter</button> 
         </div>
-        
-        
         <textarea value={value} onChange={handleChange} onInput={handleResize} className='commentbox'placeholder='Mời bạn thảo luận.... Vui lòng không spam,... để tránh bị khóa tài khoản'/>
-    </div>
+        <div className="Logo">COMICSWORLD</div>
+        
+      </div >
     
       
   );
 }
 
-export default App;
+export default Comic;
