f619f1d99618c1d5d50508f42432f6436c1aa7b1
ZzHopezZ <trananhoanguyen@gmail.com>
2023-08-16 15:22:26 +0700

    them returnComicsByUploader, ham lien quan den group

--- be/src/model/group.js
+++ be/src/model/group.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const user = require('./user')
 const groupSchema= new mongoose.Schema ({
     groupName: String,
     Description: String,
@@ -7,4 +7,27 @@ const groupSchema= new mongoose.Schema ({
 })
 const group = mongoose.model('Group', groupSchema)
 
-module.exports= group;
+async function createGroup(name, description)
+{
+    // const checkUploader= await user.findOne({username:uploader})
+    const newGroup= new group({groupName: name, Description: description})
+    newGroup.save()
+    return true
+}
+
+async function addMemberToGroup(nameUploader, nameGroup)
+{
+    const checkGroup = await group.findOne({groupName: nameGroup})
+    if(checkGroup)
+    {   
+        const checkMember = await user.findOne({username: nameUploader, Role:"uploader"})
+        if(checkMember)
+        {  
+            const newMemberGroup = await group.updateOne({groupName: nameGroup}, {$addToSet:{Uploader:[nameUploader]}})
+            return true
+        }
+    }
+    return false
+}
+
+module.exports= {group, createGroup, addMemberToGroup};
