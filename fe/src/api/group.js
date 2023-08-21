68ef1518abb6dd25ac3967019a8e371fcd4cd947
Toan Golden Pham <toanphamphu2003@gmail.com>
2023-08-21 17:48:10 +0700

    add member to group api

--- fe/src/api/group.js
+++ fe/src/api/group.js
@@ -2,12 +2,12 @@ import {axiosClient} from "./axiosConnect"
 
 async function createGroup(nameGroup,description) {
     let link = '/group/createGroup'
-    axiosClient.post(link,{nameGroup:nameGroup,description:description })
+    return axiosClient.post(link,{nameGroup:nameGroup,description:description })
 }
 
 async function addMemberToGroup(nameGroup,nameUploader,) {
     let link = '/group/addMemberToGroup'
-    axiosClient.post(link,{nameGroup:nameGroup, nameUploader:nameUploader })
+    return axiosClient.post(link,{nameGroup:nameGroup, nameUploader:nameUploader })
 }
 
 export {
