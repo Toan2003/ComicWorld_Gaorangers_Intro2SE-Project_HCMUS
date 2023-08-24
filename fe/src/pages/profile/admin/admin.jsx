291d1bcf4c891134a292c9fc2979115b2e173f46
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2023-08-24 12:48:22 +0700

    add create group

--- fe/src/pages/profile/admin/admin.jsx
+++ fe/src/pages/profile/admin/admin.jsx
@@ -15,11 +15,15 @@ export default function Admin({ handleLogout, setIsOnManageAccount }) {
                     <Link className='link-item' onClick={() => setIsOnManageAccount(true)}>Quản lý account</Link>
                 </li>
                 <li>
-                    <Link className='link-item' onClick={() => setIsOnManageAccount(false)} to='/profile/create-group'>Đăng kí tác giả</Link>
+                    <Link className='link-item' onClick={() => setIsOnManageAccount(false)} to='/profile/create-group'>Đăng kí nhóm tác giả</Link>
+                </li>
+                <li>
+                    <Link className='link-item' onClickCapture={() => setIsOnManageAccount(false)}>Thêm thành viên</Link>
                 </li>
                 <li>
                     <Link className='link-item' to='/' onClick={handleLogout}>Thoát</Link>
                 </li>
+                <li></li>
             </ul>
             <Outlet />
         </>)
