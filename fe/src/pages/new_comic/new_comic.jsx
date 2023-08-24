0ef327eb1bb18748558a77530dabd13bd2903a36
To Khanh Linh <lintoh111@gmail.com>
2023-08-24 10:14:45 +0700

    css new comic tai anh

--- fe/src/pages/new_comic/new_comic.jsx
+++ fe/src/pages/new_comic/new_comic.jsx
@@ -2,11 +2,12 @@ import './style.css';
 import Popup from 'reactjs-popup';
 
 import { Link, Outlet,useNavigate} from 'react-router-dom'
-import { FaHome, FaListUl, FaChevronRight, FaChevronLeft, FaHeart, FaChevronDown, FaRegWindowClose } from "react-icons/fa"
-import React,{useState} from "react"
+import {FaUpload } from "react-icons/fa"
+import React,{useState, useRef} from "react"
 import {postCreateComic} from "../../api/comic"
 import { underline } from '@cloudinary/url-gen/qualifiers/textDecoration';
 import {convertBase64} from '../../api/convertImage'
+import { BsImage } from "react-icons/bs";
 function NewComic() {
   const [TypesList, SetTypeList] = useState([
     'Lãng mạng',
@@ -28,7 +29,7 @@ function NewComic() {
   const[Complete, setComplete]=useState(true);
   const[Drop, setDrop]=useState(true);
   const [Description, setDescription] = useState("") //description
-  const[file,setFiles]= useState()
+  
   const[Date,setDate]=useState()
   const navigate = useNavigate();
   const handleChange_Description= (event) => {
@@ -46,6 +47,8 @@ function NewComic() {
   {
     navigate('/');
   }
+  const [file, setFile] = useState();
+  const inputRef = useRef(null);
   const SendData = async(event) => //Hàm button ở đây đã lấy đủ dữ liệu
   {
     // console.log(Name)
@@ -55,23 +58,32 @@ function NewComic() {
     // console.log(Select_state)
     // console.log(file)
     const id = localStorage.getItem('id')
-    if(Name==""||Author==""||selects_Type==""||Date==null||Select_state==""
-    ||file==null){
-      alert("Thông tin trống vui lòng nhập lại!")
-    }
-    let f = await convertBase64(file[0])
-    // console.log(f)
-    let result = await postCreateComic(Name,Date,Author, id,selects_Type,Select_state,f)
-    // console.log(result.data.data.isSuccess)
-    if (!result.data.isSuccess) {
-      alert("Thông tin sai vui lòng nhập lại!")
+    if(Name==""||Author==""||selects_Type==""||Date==null||Select_state==""||file==null){
+        alert("Thông tin trống vui lòng nhập lại!")
     }
     else
     {
-      alert("Upload thành công!")
-      navigate_to('/');
+        let f = await convertBase64(file[0])
+        let result = await postCreateComic(Name,Date,Author, id,selects_Type,Select_state,f)
+        // console.log(result.data.data.isSuccess)
+        if (!result.data.isSuccess) {
+          alert("Thông tin sai vui lòng nhập lại!")
+        }
+        else
+        {
+          alert("Upload thành công!")
+          navigate_to('/');
+        }
     }
   }
+    const handleFileChange = (e) => {
+        setFile(e.target.files);
+    };
+
+    const handleUploadClick = () => {
+        inputRef.current?.click();
+    };
+
   return (
     
     <div className="new_comic">
@@ -100,6 +112,7 @@ function NewComic() {
       <input type="text" className="Authorinput" value={Author} onChange={(e)=>setAuthor(e.target.value)}/>
       <div className='Status'>Thể Loại</div>
       <select className="Select-1" value ={selects_Type} onChange={(e)=>setSelects_Type(e.target.value)}>
+          <option></option>
           {TypesList.map((Type, index) => (
                         <option >{Type}</option>
                       ))}
@@ -110,17 +123,57 @@ function NewComic() {
       </div>
       <div className='Status'> Tình trạng</div>
        <select className="Select-1" value ={Select_state} onChange={(e)=>set_Selects_state(e.target.value)}>
+       <option></option>
        {StateList.map((State, index) => (
                         <option >{State}</option>
                       ))}
       </select> 
       <div className='Status'> Bìa truyện</div>
-      <input className='Input_file' type='file' onChange={(e)=>setFiles(e.target.files)}/>
-      <div class="Button_group">
+      <div>
+      <div className="upload_chapter-upload">
+                    <h3 className="upload_chapter-sub-title">File truyện</h3>    
+                    <span className="upload_chapter-upload-wrap">
+                        <input 
+                            onChange={(e) => handleFileChange(e)} 
+                            type='file' 
+                            className="upload_chapter-file_input" 
+                            accept="image/*" 
+                            ref={inputRef}
+                        />
+
+                        {
+                            file ?
+                            (
+                                <>
+                                    <div className="upload_chapter-file-box">
+                                        {
+                                            Object.keys(file).map((obj, i) => 
+                                            <div className="upload_chapter-file" key={i}>
+                                                <p className="upload_chapter-file-text">{file[obj].name}</p>
+                                            </div>)
+                                        }
+                                    </div>
+                                    <button className='upload_chapter-button' onClick={handleUploadClick}>Chọn lại</button>
+                                </>
+                            )
+                                :
+                            (
+                                <>
+                                    <BsImage className="upload_chapter-upload-icon"/>
+                                    <button className='upload_chapter-button' onClick={handleUploadClick}>Tải ảnh</button>
+                                </>
+                            )
+                        }
+                        
+                    </span>
+                </div>
+      </div >
+      <div className="Button_group">
             <button className='Button_accept' onClick={SendData} >Save</button>
             <button className='Button_accept' onClick={navigate_to_home}>Cancel</button>
       </div>
     </div>
+
     </div>
   );
 }
